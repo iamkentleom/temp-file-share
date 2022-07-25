55196dbb815bc361e309408f80e59344d68f4486
Kent Leo L. Makibulan <leomakibulan@gmail.com>
2022-07-25 23:50:15 +0800

    feat(about): about section
    Signed-off-by: Kent Leo L. Makibulan <leomakibulan@gmail.com>

---
+++ src/components/About.svelte
@@ -0,0 +1,17 @@
+<script>
+  import AboutCard from "./AboutCard.svelte";
+</script>
+
+<div id="about" class="max-w-screen-2xl mx-auto my-32">
+  <div class="flex flex-col items-center justify-center">
+    <p class="font-extrabold text-blue-800">ABOUT</p>
+    <h2 class="text-3xl font-bold">Share files like never before</h2>
+    <p>it's this easy</p>
+    <div class="grid grid-cols-2 gap-x-40 gap-y-16 py-12">
+      <AboutCard id="one" />
+      <AboutCard id="two" />
+      <AboutCard id="three" />
+      <AboutCard id="four" />
+    </div>
+  </div>
+</div>
---
+++ src/components/AboutCard.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { Icon, CloudUpload, Qrcode, Share, Trash } from "svelte-hero-icons";
+  export let id;
+
+  const content = {
+    one: {
+      icon: CloudUpload,
+      title: "Upload file",
+      description:
+        "Upload files you wish to share. Files are uploaded to our server. No need for your own space.",
+    },
+    two: {
+      icon: Qrcode,
+      title: "Scan QRCode",
+      description: "Scan generated QRCode to download the files.",
+    },
+    three: {
+      icon: Share,
+      title: "Share file",
+      description: "Share the link to anyone. And they can download the file.",
+    },
+    four: {
+      icon: Trash,
+      title: "No worries",
+      description:
+        "Files are deleted after a day. So you'll never worry where your files go.",
+    },
+  };
+</script>
+
+<div class="max-w-sm">
+  <Icon src={content[id].icon} size="48" class="text-blue-800" />
+  <h3 class="text-2xl font-bold mt-1 mb-2">{content[id].title}</h3>
+  <p class="text-lg max-w-xs">
+    {content[id].description}
+  </p>
+</div>
