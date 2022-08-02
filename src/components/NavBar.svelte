aef9ef43d50b1f1789b182616e988006bd810814
Kent Leo L. Makibulan <leomakibulan@gmail.com>
2022-08-02 21:53:45 +0800

    feat(NavBar): initial navbar component

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Icon, ArrowSmRight } from "svelte-hero-icons";
+</script>
+
+<nav
+  class="w-full max-w-screen-2xl mx-auto py-4 flex justify-between items-center"
+>
+  <a href="." class="text-xl font-extrabold hover:text-gray-700"
+    >TempFileShare</a
+  >
+  <a
+    href="#about"
+    class="group transition text-white px-2 hover:text-gray-300 flex items-center gap-1"
+    >About <Icon
+      src={ArrowSmRight}
+      size="20"
+      class="inline group-hover:translate-x-1 transition"
+    /></a
+  >
+</nav>
