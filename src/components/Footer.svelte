9b9ab07d2954af42136c780f3deee7480c5ffa18
Kent Leo L. Makibulan <leomakibulan@gmail.com>
2022-08-02 22:00:10 +0800

    feat(Footer): initial footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,31 @@
+<footer
+  class="w-full max-w-screen-2xl mx-auto py-4 flex justify-between items-center mt-auto"
+>
+  <p class="">&copy; 2022 TempFileShare - Kent Leo L. Makibulan</p>
+  <ul class="flex justify-between items-center gap-16">
+    <li>
+      <a
+        class="hover:text-gray-500"
+        href="https://github.com/iamkentleom/temp-file-share#data-policy"
+        target="_blank"
+        rel="noreferrer">Data Policy</a
+      >
+    </li>
+    <li>
+      <a
+        class="hover:text-gray-500"
+        href="https://github.com/iamkentleom/temp-file-share#terms-and-conditions"
+        target="_blank"
+        rel="noreferrer">Terms and Conditions</a
+      >
+    </li>
+    <li>
+      <a
+        class="hover:text-gray-500"
+        href="https://github.com/iamkentleom/temp-file-share#attributions"
+        target="_blank"
+        rel="noreferrer">Attributions</a
+      >
+    </li>
+  </ul>
+</footer>
