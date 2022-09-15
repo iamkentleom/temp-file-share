ad577262dd4e41c2c84904ecc950333c120a6aa6
Kent Leo L. Makibulan <leomakibulan@gmail.com>
2022-09-15 18:02:42 +0800

    feat(App): initial app outline

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,13 @@
-<main>
-  <h1 class="text-teal-500 text-2xl font-bold">Hello</h1>
-</main>
+<script>
+  import NavBar from "./components/NavBar.svelte";
+  import Card from "./components/Card.svelte";
+  import About from "./components/About.svelte";
+  import Footer from "./components/Footer.svelte";
+</script>
+
+<div id="main" class="flex flex-col min-h-full relative">
+  <NavBar />
+  <Card />
+  <About />
+  <Footer />
+</div>
