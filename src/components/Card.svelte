00e0809b94504ee8800c9609ed247d70e8914ada
Kent Leo L. Makibulan <leomakibulan@gmail.com>
2022-09-26 21:53:29 +0800

    feat(Card): initial card component

---
+++ src/components/Card.svelte
@@ -0,0 +1,11 @@
+<script>
+  import CardTitle from "./CardTitle.svelte";
+  import CardDropzone from "./CardDropzone.svelte";
+</script>
+
+<div
+  class="w-full max-w-screen-lg mx-auto min-h-[640px] my-8 p-4 rounded-xl grid grid-cols-2 bg-white shadow-xl"
+>
+  <CardTitle />
+  <CardDropzone />
+</div>
---
+++ src/components/CardTitle.svelte
@@ -0,0 +1,11 @@
+<script>
+  import logo from "../assets/logo.svg";
+</script>
+
+<div class="flex flex-col items-center justify-around p-2">
+  <div class="flex flex-col items-center pb-20">
+    <img src={logo} alt="logo" class="w-20 h-20" />
+    <h1 class="text-4xl font-bold">TempFileShare</h1>
+    <p class="text-lg">share disappering files to anyone</p>
+  </div>
+</div>
