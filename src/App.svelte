9974561acb05033e5e06144265adc86f91cd95cb
Kent Leo L. Makibulan <leomakibulan@gmail.com>
2023-04-27 00:16:19 +0800

    feat(auth): firebase log in anonymously

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,16 @@
   import Card from "./components/Card.svelte";
   import About from "./components/About.svelte";
   import Footer from "./components/Footer.svelte";
+  import { auth, signInAnonymously } from "./firebase/auth";
+
+  signInAnonymously(auth)
+    .then(() => {
+      console.log("Logged in anonymously.");
+    })
+    .catch((e) => {
+      console.log("Code: ", e.code);
+      console.log("Message: ", e.message);
+    });
 </script>
 
 <div id="main" class="flex flex-col min-h-full relative">
