b9a66cd00131de3ec4043ef89c6c1fc616000339
Kent Leo L. Makibulan <leomakibulan@gmail.com>
2023-04-27 00:17:27 +0800

    fix(store): move folder name handling in store

--- src/stores.js
+++ src/stores.js
@@ -1,5 +1,7 @@
 import { writable } from "svelte/store";
+import { nanoid } from "nanoid";
 
 export const files = writable([]);
 export const values = writable();
 export const toastState = writable(false);
+export const folder = writable(nanoid(12));
