5977e8d978695622a0db977a354787926d2ad289
Kent Leo L. Makibulan <leomakibulan@gmail.com>
2023-05-19 18:48:28 +0800

    fix(footer): mobile version

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,10 +1,12 @@
 <footer
-  class="w-full max-w-screen-2xl mx-auto px-8 py-6 flex justify-between items-center mt-auto"
+  class="w-full max-w-screen-2xl mx-auto px-8 py-6 flex flex-col-reverse lg:flex-row gap-8 lg:gap-0 justify-between items-center mt-auto"
 >
-  <p class="">
+  <p class="text-center text-sm md:text-base">
     &copy; {new Date().getFullYear()} TempFileShare - Kent Leo L. Makibulan
   </p>
-  <ul class="flex justify-between items-center gap-16">
+  <ul
+    class="flex flex-col md:flex-row justify-between items-center gap-2 md:gap-16"
+  >
     <li>
       <a
         class="hover:text-gray-500"
